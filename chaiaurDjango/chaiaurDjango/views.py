9eef4a6aae08764ab9f58af80578ea3b8d5fa57c
durjoy_10 <ug2102017@cse.pstu.ac.bd>
2024-12-10 10:16:07 +0600

    durjoy

---
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Home page")
+    return render(request,'website/index.html')
+
+def about(request):
+    return HttpResponse("about page")
+
+def contact(request):
+    return HttpResponse("contact page")
+
