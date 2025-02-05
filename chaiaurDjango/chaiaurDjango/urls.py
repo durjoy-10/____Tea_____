3beb4e7977404f4cfdc26a577dbd659096d7edd4
durjoy-10 <ug2102017@cse.pstu.ac.bd>
2025-02-05 09:00:31 +0600

    durjoy

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('contact/',views.contact,name='Contact'),
     path('chai/',include('chai.urls')),
     path('Reviews/',include('Reviews.urls')),
-    
-    
+ 
     
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
